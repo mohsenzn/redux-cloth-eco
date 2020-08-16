857b5c6a940373e34629f70f5075eaea79e7439c
unknown <mohsen.zergani@gmail.com>
2020-08-16 18:47:33 +0430

    create product menu

--- src/components/Preview/Preview-collection.js
+++ src/components/Preview/Preview-collection.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-const PreviewCollction = ({title,items}) => {
+import CollectionItem from '../colleITems'
+const PreviewCollction = ({ title, items }) => {
     return (
         <div className="collection-preview">
-            <div className="title">{title.toUpperCase()}</div>
+            <h1 className="title">{title.toUpperCase()}</h1>
             <div className="preview">
                 {
-                    items.map(item => (
-                    <div key={item.id}>{item.name}</div>
+                    items
+                    .filter((item, index) => index < 4)
+                    .map(({id,...otherItemProps}) => (
+                        <CollectionItem key={id} {...otherItemProps}/>
                     ))
                 }
             </div>
---
+++ src/components/colleITems/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const index = ({ id, name, price, imageUrl }) => {
+    return (
+        <>
+            <div className="collection-item">
+                <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+                <div className="collection-footer">
+                    <span className="name">{name}</span>
+                    <span className="price">{price}</span>
+
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default index;
